72c2f023d758f32717b36bdac83966cb64be3cb1
Mathew Sida-Nguon <sida.mathew@gmail.com>
2023-01-26 18:36:40 -0800

    Saving

---
+++ index.js
@@ -0,0 +1,327 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const generateHTML = require('./src/generateHTML');
+
+
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const staffData = [];
+
+const createManager = () => 
+{
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the team managers name?',
+            name: 'name',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter a name.';
+
+                } else {
+                    return true;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            message: 'What is the team managers id?',
+            name: 'id',
+            validate: (answer) => 
+            {
+
+                if (!answer) {
+                    return 'please enter an id.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team managers email?',
+            name: 'email',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter an email.';
+
+                } 
+                else 
+                {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team managers office number?',
+            name: 'officeNumber',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter an office number.';
+
+                } else {
+                    return true;
+                }
+            }
+        }
+
+    ]).then((answers) => 
+    {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        staffData.push(manager);
+
+        console.log(manager);
+        addingTeam();
+
+
+    })
+}
+
+function addingTeam() 
+{
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Would you like to add another team member',
+            name: 'addMember'
+        }
+    ]).then((answers) => 
+    {
+        if (answers.addMember === true) 
+        {
+            chooseRole();
+        } else {
+            fs.writeFile('index.html', generateHTML, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!'))
+   
+        }
+    })
+}
+
+function chooseRole() 
+{
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What type of team member would you like to add?',
+            name: 'roles',
+
+            choices: [Engineer, Intern]
+        }
+    ]).then((answers) => {
+    if (answers.roles === 'Engineer') 
+    {
+    createEngineer();
+    }
+    if(answers.roles === 'Intern') 
+    {
+        createIntern();
+    }
+})
+
+}
+
+function createIntern() 
+{
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the team interns name?',
+            name: 'name',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter a name.';
+
+                } else 
+                {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team interns id?',
+            name: 'id',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter an id.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team interns email?',
+            name: 'email',
+            validate: (answer) => 
+            {
+
+                if (!answer) {
+                    return 'please enter an email.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the school they attend?',
+            name: 'school',
+            validate: (answer) => 
+            {
+
+                if (!answer) 
+                {
+                    return 'please enter a school.';
+
+                } else 
+                {
+                    return true;
+                }
+            }
+        }
+
+    ]).then((answers) => 
+    {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        staffData.push(intern);
+        addingTeam();
+    })
+}
+
+function createEngineer() 
+{
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the team engineers name?',
+            name: 'name',
+            validate: (answer) => 
+            {
+
+                if (!answer) {
+                    return 'please enter a name.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team engineers id?',
+            name: 'id',
+            validate: (answer) => 
+            {
+
+                if (!answer) {
+                    return 'please enter an id.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team engineers email?',
+            name: 'email',
+            validate: (answer) => {
+
+                if (!answer) {
+                    return 'please enter an email.';
+
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the team engineers Github?',
+            name: 'github',
+            validate: (answer) => {
+
+                if (!answer) {
+                    return 'please enter a github username';
+
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then((answers) => 
+    {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        staffData.push(engineer);
+        addingTeam();
+    })
+}
+
+const staffCards = []
+
+const cardMaker = (staffData) => 
+{
+    let memberRole = ``
+    staffData.forEach((person) => 
+    {
+
+        if (person.role === 'Manager') 
+        {
+            memberRole = `Office Number: ${person.officeNumber}`
+        }
+        if (person.role === 'Engineer') 
+        {
+            memberRole = `Github: ${person.github}`
+        }
+        if (person.role === 'Intern') 
+        {
+            memberRole = `School: ${person.school}`
+        }
+
+        let card = ` <div class="card employee-card">
+        <div class="card-head">
+            <h2 class="card-title">${person.name}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2">${person.role}</i></h3>
+        </div>
+        <div class="card-body">
+            <ul class="info">
+               <li class="info-item">${person.id}</li>
+               <li class="info-item">${person.email}</li>
+               <li class="info-item">${memberRole}</li>
+            </ul>
+        </div>
+    </div>`
+
+        staffCards.push(card)
+    })
+    console.log(staffCards)
+}
+createManager();
---
+++ source/HTMLGen.js
@@ -0,0 +1,41 @@
+const generateHTML =(staffCards) =>{ `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 p-5 mb-3 team-header">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-group col-12 d-flex justify-content-center">
+        ${staffCards}
+        
+        
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+</html>`
+}
+
+module.exports = generateHTML;
